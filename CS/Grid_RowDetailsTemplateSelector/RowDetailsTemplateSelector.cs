1736fab1d602a8f8c542740a380a5d8950c1c7a5
AndreySlabov <andrey.slabov@devexpress.com>
2022-06-22 18:10:09 +0400

    Update sample

--- CS/Grid_RowDetailsTemplateSelector/RowDetailsTemplateSelector.cs
+++ CS/Grid_RowDetailsTemplateSelector/RowDetailsTemplateSelector.cs
@@ -1,13 +1,9 @@
 ﻿using DevExpress.Xpf.Grid;
-using System;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace Grid_RowDetailsTemplateSelector
-{
-    class RowDetailsTemplateSelector : DataTemplateSelector
-    {
+namespace Grid_RowDetailsTemplateSelector {
+    class RowDetailsTemplateSelector : DataTemplateSelector {
         public DataTemplate PrimaryRowDetailsTemplate { get; set; }
         public DataTemplate SecondaryRowDetailsTemplate { get; set; }
         public override DataTemplate SelectTemplate(object item, DependencyObject container) {
